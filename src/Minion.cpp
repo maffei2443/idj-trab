e8cde1bdd2e0db4e59730d3435da008cc6afa2ce
maffei2443 <leoitu22hotmail.com@gmail.com>
2019-05-08 14:23:04 -0300

    minion em torno do alien

--- src/Minion.cpp
+++ src/Minion.cpp
@@ -15,7 +15,9 @@ Minion::Minion (GameObject& associated, weak_ptr<GameObject> alienCenter,
     new Sprite(this->associated, "assets/img/minion.png");
     
     // this->associated.box.SetXY(this->alienCenter.box.x, this->alienCenter.box.y);
-    this->associated.box.SetXY(30, 30);
+    int x = 40;
+    this->associated.box.SetXY(x+this->alienCenter.box.x
+                            , x+this->alienCenter.box.y);
     // printf("MINION X,Y %lf %lf\n", this->alienCenter.box.x, this->alienCenter.box.y);
     
     cout << "Minion.associated --> " << &this->associated << endl;
@@ -31,44 +33,19 @@ Minion::~Minion () {
 void Minion::Update(double dt) {
     (void)dt;
     // printf("[Minion.Update] %p\n", this);
-    this->associated.box /= 3;
     Vec2 centerOfAlien = this->alienCenter.box.center;
 
-    // Vec2 newMinionPos(
-    //     (this->associated.box.x - centerOfAlien.x),
-    //     (this->associated.box.y - centerOfAlien.y)
-    // );
-
-    Vec2 pos = {this->associated.box.x, this->associated.box.y};
+    Vec2 pos = {this->associated.box.x - this->alienCenter.box.x
+                , this->associated.box.y - this->alienCenter.box.y};
     pos.rotate(0.07);
-    cout << "Pos : " << pos;
-    pos *= 3;
+    pos.x += this->alienCenter.box.x;
+    pos.y += this->alienCenter.box.y;
     cout << "Alien Center: " << this->alienCenter.box.center << endl;
     this->associated.box.SetXY(pos.x, pos.y);
-    // Vec2 newMinionPos = Vec2(this->associated.box.x, this->associated.box.y);
-
-    // newMinionPos.rotate( 0.07 );
-
-
-    // this->associated.box.x = (newMinionPos.x + centerOfAlien.x);
-    printf("this->associated.box.x ------------ %lf\n", this->associated.box.x);
-    // this->associated.box.x /= 2;
-    printf("this->associated.box.x/2 ------------ %lf\n", this->associated.box.x/2);
-
-    // this->associated.box.y = (newMinionPos.y + centerOfAlien.y);
-    // printf("this->associated.box.y ------------ %lf\n", this->associated.box.y);
-
-    // Vec2 newMinionPos(this->associated.box.x, this->associated.box.y);
-
-    // newMinionPos.rotate( 0.01 );
-
-    // Vec2 alienCenter = this->alienCenter.box.center;
-
-    // this->associated.box.x = newMinionPos.x;
-
-    // this->associated.box.y = newMinionPos.y;
-
+    // printf("this->associated.box.x ------------ %lf\n", this->associated.box.x);
+    // printf("this->associated.box.x/2 ------------ %lf\n", this->associated.box.x/2);
 }
+
 void Minion::Render(){ 
 
 }
