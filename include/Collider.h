4e9633f4ab08be52742826290bf0e129b48a9454
maffei2443 <leoitu22hotmail.com@gmail.com>
2019-05-19 14:59:22 -0300

    add collisioner

---
+++ include/Collider.h
@@ -0,0 +1,32 @@
+#ifndef COLLIDER_H
+#define COLLIDER_H
+
+#include <string>
+#include "Component.h"
+#include "GameObject.h"
+#include "Vec2.h"
+using std::string;
+
+class Collider : public Component {
+private:
+    Vec2 scale;
+    Vec2 offset;
+    const static string type;
+public:
+    Collider(GameObject&, Vec2 scale = {1, 1}, Vec2 offset = {0, 0});
+    ~Collider();
+    void Update(double dt);
+    void Render();
+    bool Is(string type) {
+        return this->type == this->type;
+    }
+    const inline string GetType() const {
+        return this->type;
+    }
+    void SetScale(Vec2 scale);
+    void SetOffset(Vec2 offset);
+    Rect box;
+    void Start();
+};
+
+#endif
---
+++ src/Collider.cpp
@@ -0,0 +1,49 @@
+#include <string>
+#include <SDL2/SDL.h>
+#include "Collider.h"
+#include "Component.h"
+#include "GameObject.h"
+#include "Vec2.h"
+#include "Camera.h"
+#include "Game.h"
+#include <iostream>
+using std::cout;
+using std::endl;
+//#define DEBUG
+const string Collider::type("Collider");
+
+Collider::Collider(GameObject& associated, Vec2 scale, Vec2 offset) : Component(associated) {
+    this->associated.AddComponent((this));
+}
+
+Collider::~Collider() {
+    cout << "Collider died!" << endl;
+}
+
+void Collider::Update(double dt) {
+    /* sete a box atual como uma cópia da box de associated mas com sua
+largura e altura multiplicados por uma escala */
+    this->box = this->associated.box;
+    this->box.w *= this->scale.x;
+    this->box.h *= this->scale.y;
+    /* centro dela seja igual ao centro da box de associated */
+    /* adicionado do atributo offset rotacionado pelo ângulo de associated. */
+    this->box.SetCenter(this->associated.box.GetCenter() + this->offset);
+}
+
+
+void Collider::Render() {
+}
+
+
+void Collider::SetOffset(Vec2 offset) {
+    this->offset = offset;
+}
+
+void Collider::SetScale(Vec2 scale) {
+    this->scale = scale;
+}
+
+void Collider::Start() {
+    this->started = true;
+}
